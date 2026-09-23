cae83d4e197c284387193fb5b63d41ddcce080df
Milan <milanmilan.mm55@gmail.com>
2026-09-23 20:38:36 +0200

    implemented recursive search

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,42 @@
+#include "../headers/app.hpp"
+#include "../headers/parsers.hpp"
+#include <filesystem>
 #include <print>
 #include <string>
+#include <sys/types.h>
 #include <unistd.h>
 #include <vector>
 
-typedef struct {
-  bool r_flag;
-  bool i_flag;
-} Flags;
+void print_app(App *app) {
+  std::println("Flags: {} {}", app->flags.r_flag, app->flags.i_flag);
+  std::println("Search path: {}", app->search_path);
+  std::println("File count: {}", app->files.size());
 
-typedef struct {
-  Flags flags;
-  std::string search_path;
-  std::vector<std::string> files;
-} App;
+  std::print("\nFiles:");
+  for (const auto &file : app->files)
+    std::print(" {}", file);
+  std::print("\n");
+}
+
+void read_dir(App *app, std::vector<std::filesystem::directory_entry> *dirs) {
+  pid_t pid = getpid();
+  auto dir = std::move(dirs->back());
+  dirs->pop_back();
+
+  for (const auto &entry : std::filesystem::directory_iterator(dir)) {
+    std::string file_name = entry.path().filename().string();
+
+    if (entry.is_directory() && app->flags.r_flag) {
+      dirs->push_back(entry);
+    }
+
+    if (!entry.is_directory()) {
+      auto it = std::find(app->files.begin(), app->files.end(), file_name);
+      if (it != app->files.end())
+        std::println("{}: {}: {}/{}", pid, *it, dir.path().string(), *it);
+    }
+  }
+}
 
 int main(int argc, char *argv[]) {
   App app = {.flags =
@@ -23,36 +47,30 @@ int main(int argc, char *argv[]) {
              .search_path = std::string(),
              .files = std::vector<std::string>()};
 
-  std::println("argc: {}", argc);
-
-  for (int i = 0; i < argc; i++) {
-    std::println("[{}]: {}", i, argv[i]);
+  if (argc < 3) {
+    std::println(
+        "myfind [-R] [-i] searchpath filename1 [filename2] [filenameN]");
+    return 0;
   }
 
-  int option;
-  while ((option = getopt(argc, argv, "Ri")) != -1) {
-    switch (option) {
-    case 'R':
-      app.flags.r_flag = true;
-      break;
+  parse_flags(argc, argv, &app);
+  parse_parameters(argc, argv, &app);
 
-    case 'i':
-      app.flags.i_flag = true;
-      break;
+  // TODO: handle error_code
+  if (!std::filesystem::is_directory(app.search_path)) {
+    std::println("Invalid path: {}", app.search_path);
+    return -1;
+  }
 
-    case '?':
-      std::println("Hellnah");
-      return 0;
+  std::vector<std::filesystem::directory_entry> dirs;
+  dirs.push_back(std::filesystem::directory_entry(app.search_path));
+
+  // TODO: Check if dir is valid
+  if (app.flags.r_flag) {
+    while (!dirs.empty()) {
+      read_dir(&app, &dirs);
     }
   }
-
-  // INFO:./myfind [-R] [-i] searchpath filename1 [filename2] [filenameN]
-  // R - toggle recursive search on and search for all matches
-  // i - toggle case sensitive off
-  // searchpath - absolute or relative path
-  // filename - plain string file name
-  // no support for paths, subpaths, wildcards required.
-  // Example: ./myfind ./ test.txt test.doc test
 
   return 0;
 }
